9f06199ef73c6103733700721f8887b19209cc1e
MOHIT GOSWAMI <mohitgoswami966@gmail.com>
2026-07-26 19:32:58 +0530

    Time: 0 ms (100%), Space: 10.1 MB (79.15%) - LeetHub

---
+++ 0198-house-robber/0198-house-robber.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int rob(vector<int>& nums) {
+        int n=nums.size();
+        int prev1=nums[0];
+        int prev2=0;
+        for(int i=1;i<n;i++){
+            int incl=prev2 +nums[i];
+            int excl=prev1+0;
+            int ans=max(incl,excl);
+            prev2=prev1;
+            prev1=ans;
+        }
+        return prev1;
+    }
+};
