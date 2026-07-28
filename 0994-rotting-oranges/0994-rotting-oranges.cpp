40230fd6907ddb491a849572fc51be7a25b6bb61
MOHIT GOSWAMI <mohitgoswami966@gmail.com>
2026-07-28 17:46:08 +0530

    Time: 0 ms (100%), Space: 17.2 MB (28.82%) - LeetHub

---
+++ 0994-rotting-oranges/0994-rotting-oranges.cpp
@@ -0,0 +1,46 @@
+class Solution {
+public:
+    int orangesRotting(vector<vector<int>>& grid) {
+        int n=grid.size();
+        int m=grid[0].size();
+        int cntFresh=0;
+        queue<pair<pair<int,int>,int>> q;
+        vector<vector<int>> visited(n,vector<int> (m,0));
+        for(int i=0;i<grid.size();i++){
+            for(int j=0;j<grid[0].size();j++){
+                if(grid[i][j]==2){
+                    q.push({{i,j},0});
+                    visited[i][j]=2;
+                }
+                else{
+                    visited[i][j]=0;
+                }
+                if(grid[i][j]==1){
+                    cntFresh++;
+                }
+            }
+        }
+        int tm=0;
+        int cnt=0;
+        int aRow[]={-1,0,1,0};
+        int aCol[]={0,1,0,-1};
+        while(!q.empty()){
+            int r=q.front().first.first;
+            int c=q.front().first.second;
+            int t=q.front().second;
+            q.pop();
+            tm=max(tm,t);
+            for(int i=0;i<4;i++){
+                int nrow=r+aRow[i];
+                int ncol=c+aCol[i];
+                if((nrow>=0 && nrow<n) && (ncol>=0 && ncol<m) && grid[nrow][ncol]==1 && !visited[nrow][ncol]){
+                    cnt++;
+                    q.push({{nrow,ncol},t+1});
+                    visited[nrow][ncol]=2;
+                }
+            }
+        }
+        if(cnt!=cntFresh) return -1;
+        return tm;
+    }
+};
