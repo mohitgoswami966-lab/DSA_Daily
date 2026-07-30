f0330ff9ccfe3a0f2d77b4525cf861ad4e94c04c
MOHIT GOSWAMI <mohitgoswami966@gmail.com>
2026-07-30 11:18:31 +0530

    Time: 0 ms (100%), Space: 8.4 MB (54.67%) - LeetHub

--- 3014-minimum-number-of-pushes-to-type-word-i/3014-minimum-number-of-pushes-to-type-word-i.cpp
+++ 3014-minimum-number-of-pushes-to-type-word-i/3014-minimum-number-of-pushes-to-type-word-i.cpp
@@ -1,9 +1,8 @@
 class Solution {
 public:
     int minimumPushes(string word) {
-        int n=word.size();
         int ans=0;
-        for(int i=0;i<n;i++){
+        for(int i=0;i<word.size();i++){
             ans +=(i/8)+1;
         }
         return ans;
