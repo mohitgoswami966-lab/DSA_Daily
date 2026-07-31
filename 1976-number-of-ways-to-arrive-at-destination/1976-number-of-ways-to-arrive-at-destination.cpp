398529ce5b987568911303b4e417294f8f4c18d2
MOHIT GOSWAMI <mohitgoswami966@gmail.com>
2026-07-31 18:26:03 +0530

    Time: 12 ms (38.46%), Space: 35.1 MB (68.41%) - LeetHub

--- 1976-number-of-ways-to-arrive-at-destination/1976-number-of-ways-to-arrive-at-destination.cpp
+++ 1976-number-of-ways-to-arrive-at-destination/1976-number-of-ways-to-arrive-at-destination.cpp
@@ -12,9 +12,9 @@ public:
         vector<long long> dist(n,LLONG_MAX);
         vector<int> ways(n,0);
         priority_queue<pair<long long,int>,vector<pair<long long,int>>,greater<pair<long long,int>>> pq;
+        pq.push({0,0});
         dist[0]=0;
         ways[0]=1;
-        pq.push({0,0});
         int mod=(int)(1e9 +7);
         while(!pq.empty()){
             long long d=pq.top().first;
