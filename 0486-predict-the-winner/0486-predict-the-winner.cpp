7e49808b471052f785544cdb793001b02e73c4d3
MOHIT GOSWAMI <mohitgoswami966@gmail.com>
2026-08-01 13:05:18 +0530

    Time: 0 ms (100%), Space: 10.3 MB (16.44%) - LeetHub

--- 0486-predict-the-winner/0486-predict-the-winner.cpp
+++ 0486-predict-the-winner/0486-predict-the-winner.cpp
@@ -1,15 +1,17 @@
 class Solution {
 public:
-    int solve(int i, int j, vector<int>& nums) {
+    int solve(int i, int j, vector<int>& nums,vector<vector<int>> &dp) {
         if (i == j)
             return nums[i];
+        if(dp[i][j]!=INT_MAX) return dp[i][j];
+        int takeLeft = nums[i] - solve(i + 1, j, nums,dp);
+        int takeRight = nums[j] - solve(i, j - 1, nums,dp);
 
-        int takeLeft = nums[i] - solve(i + 1, j, nums);
-        int takeRight = nums[j] - solve(i, j - 1, nums);
-
-        return max(takeLeft, takeRight);
+        return dp[i][j]=max(takeLeft, takeRight);
     }
     bool predictTheWinner(vector<int>& nums) {
-        return solve(0, nums.size() - 1, nums) >= 0;
+        int n=nums.size();
+        vector<vector<int>> dp(n,vector<int>(n,INT_MAX));
+        return solve(0, nums.size() - 1, nums,dp) >= 0;
     }
 };
