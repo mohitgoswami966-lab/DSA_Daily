2033826b47dc13760ef492d1d2d5ee8863bd7066
MOHIT GOSWAMI <mohitgoswami966@gmail.com>
2026-08-03 17:02:21 +0530

    Time: 0 ms (100%), Space: 16.4 MB (77.98%) - LeetHub

--- 0909-snakes-and-ladders/0909-snakes-and-ladders.cpp
+++ 0909-snakes-and-ladders/0909-snakes-and-ladders.cpp
@@ -1,29 +1,27 @@
 class Solution {
 public:
     int snakesAndLadders(vector<vector<int>>& board) {
-        int n = board.size();
-        vector<bool> vis(n * n + 1, false);
-        queue<pair<int, int>> q;
-        q.push({1, 0});
-        vis[1] = true;
-        while (!q.empty()) {
-            auto [curr, dist] = q.front();
+        int n=board.size();
+        vector<bool> visited(n*n+1,false);
+        queue<pair<int,int>> q;
+        q.push({1,0});
+        visited[1]=true;
+        while(!q.empty()){
+            int curr=q.front().first;
+            int dist=q.front().second;
             q.pop();
-            if (curr == n * n)
-                return dist;
-            for (int dice = 1; dice <= 6 && curr + dice <= n * n; dice++) {
-                int next = curr + dice;
-                int x = next - 1;
-                int r = x / n;
-                int c = x % n;
-                int row = n - 1 - r;
-                if (r % 2 == 1)
-                    c = n - 1 - c;
-                if (board[row][c] != -1)
-                    next = board[row][c];
-                if (!vis[next]) {
-                    vis[next] = true;
-                    q.push({next, dist + 1});
+            if(curr==n*n) return dist;
+            for(int dice=1;dice<=6 && curr+dice<=n*n;dice++){
+                int next=curr+dice;
+                int x=next-1;
+                int r=x/n;
+                int c=x%n;
+                int row=n-1-r;
+                if(r%2==1) c=n-1-c;
+                if(board[row][c]!=-1) next=board[row][c];
+                if(!visited[next]){
+                    visited[next]=true;
+                    q.push({next,dist+1});
                 }
             }
         }
