0253b035fb29ad0317650a8d5096165b74c515ca
MOHIT GOSWAMI <mohitgoswami966@gmail.com>
2026-08-11 15:42:16 +0530

    Time: 0 ms (100%), Space: 25 MB (78.16%) - LeetHub

---
+++ 3046-split-the-array/3046-split-the-array.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    bool isPossibleToSplit(vector<int>& nums) {
+        int n=nums.size();
+        vector<int> freq(101,0);
+        for(int i=0;i<nums.size();i++){
+            freq[nums[i]]++;
+        }
+        for(int i=0;i<nums.size();i++){
+            if(freq[nums[i]]>2) return false;
+        }
+        return true;
+    }
+};
