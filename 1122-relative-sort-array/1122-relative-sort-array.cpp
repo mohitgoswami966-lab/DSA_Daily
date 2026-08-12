ae1671728663589a57b11352e0c755768cfec24f
MOHIT GOSWAMI <mohitgoswami966@gmail.com>
2026-08-12 14:10:26 +0530

    Time: 0 ms (100%), Space: 11.1 MB (67.26%) - LeetHub

--- 1122-relative-sort-array/1122-relative-sort-array.cpp
+++ 1122-relative-sort-array/1122-relative-sort-array.cpp
@@ -1,28 +1,22 @@
 class Solution {
 public:
     vector<int> relativeSortArray(vector<int>& arr1, vector<int>& arr2) {
-        int n=arr1.size();
         vector<int> ans;
-        vector<int> a1;
-        unordered_set<int> s(arr2.begin(),arr2.end());
-        unordered_map<int,int> freq;
+        vector<int> freq(1001,0);
         for(int i=0;i<arr1.size();i++){
             freq[arr1[i]]++;
         }
         for(int i=0;i<arr2.size();i++){
-            int a=arr2[i];
-            while(freq[a]--){
-                ans.push_back(a);
+            while(freq[arr2[i]]>0){
+                ans.push_back(arr2[i]);
+                freq[arr2[i]]--;
             }
         }
-        for(int i=0;i<arr1.size();i++){
-            if(s.find(arr1[i])==s.end()){
-                a1.push_back(arr1[i]);
+        for(int i=0;i<1001;i++){
+            while(freq[i]>0){
+                ans.push_back(i);
+                freq[i]--;
             }
-        }
-        sort(a1.begin(),a1.end());
-        for(int i=0;i<a1.size();i++){
-            ans.push_back(a1[i]);
         }
         return ans;
     }
