7307ace76bfd7a72fadfd53c078aa59ee0528eea
MOHIT GOSWAMI <mohitgoswami966@gmail.com>
2026-08-14 15:24:29 +0530

    Time: 3 ms (22.34%), Space: 25.8 MB (96.24%) - LeetHub

--- 0922-sort-array-by-parity-ii/0922-sort-array-by-parity-ii.cpp
+++ 0922-sort-array-by-parity-ii/0922-sort-array-by-parity-ii.cpp
@@ -1,26 +1,15 @@
 class Solution {
 public:
     vector<int> sortArrayByParityII(vector<int>& nums) {
-        int n=nums.size();
-        vector<int> ans(n);
-        stack<int> even;
-        stack<int> odd;
-        for(int i=0;i<nums.size();i++){
-            if(nums[i]%2==0) even.push(nums[i]);
-            else{
-                odd.push(nums[i]);
+        int i = 0; int j = 1; int n = nums.size();
+        while (i < n && j < n) {
+            if(nums[i] % 2 == 0) i+=2;
+            else if (nums[j] % 2 == 1) j+=2;
+            else {
+                (swap(nums[i], nums[j]));
+                i+=2, j+=2;
             }
         }
-        for(int i=0;i<nums.size();i++){
-            if(i%2==0){
-                ans[i]=even.top();
-                even.pop();
-            }
-            else{
-                ans[i]=odd.top();
-                odd.pop();
-            }
-        }
-        return ans;
+        return nums;
     }
 };
