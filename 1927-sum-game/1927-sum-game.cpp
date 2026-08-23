b341aad668b06458d8509838aa785123b38a46ee
MOHIT GOSWAMI <mohitgoswami966@gmail.com>
2026-08-23 08:56:12 +0530

    Time: 4 ms (47.55%), Space: 13.9 MB (67.83%) - LeetHub

---
+++ 1927-sum-game/1927-sum-game.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    bool sumGame(string num) {
+        double res=0;
+        double n=num.size();
+        for(int i=0;i<n;i++){
+            double sign=0;
+            if(i<n/2) sign=1;
+            else sign=-1;
+            double value;
+            if(num[i]=='?') value=4.5;
+            else value=num[i]-'0';
+            res+=sign*value;
+        }
+        if(res!=0.0) return true;
+        return false;
+    }
+};
