5004f0f76bbc19f431726b53ac9f39342b33195e
MOHIT GOSWAMI <mohitgoswami966@gmail.com>
2026-08-23 23:12:28 +0530

    Time: 3 ms (54.45%), Space: 11.1 MB (68.16%) - LeetHub

---
+++ 1039-minimum-score-triangulation-of-polygon/1039-minimum-score-triangulation-of-polygon.cpp
@@ -0,0 +1,22 @@
+class Solution {
+private:
+    int solve(vector<int> &val){
+        int n=val.size();
+        vector<vector<int>> dp(n,vector<int>(n,0));
+        for(int i=n-1;i>=0;i--){
+            for(int j=i+2;j<n;j++){
+                int ans=INT_MAX;
+                for(int k=i+1;k<j;k++){
+                    ans=min(ans,val[i]*val[j]*val[k]+dp[i][k]+dp[k][j]);
+                }
+                dp[i][j]=ans;
+            }
+        }
+        return dp[0][n-1];
+    }
+public:
+    int minScoreTriangulation(vector<int>& values) {
+        int n=values.size();
+        return solve(values);
+    }
+};
