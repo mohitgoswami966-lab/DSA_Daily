334dadce9221da870059d6588d82797eef42f2fe
MOHIT GOSWAMI <mohitgoswami966@gmail.com>
2026-08-28 18:42:09 +0530

    Time: 0 ms (100%), Space: 8 MB (43.67%) - LeetHub

---
+++ 0096-unique-binary-search-trees/0096-unique-binary-search-trees.cpp
@@ -0,0 +1,17 @@
+class Solution {
+private:
+    int solve(int n,vector<int> &dp){
+        if(n<=1) return 1;
+        int ans=0;
+        if(dp[n]!=-1) return dp[n];
+        for(int i=1;i<=n;i++){
+            ans +=solve(i-1,dp)*solve(n-i,dp);
+        }
+        return dp[n]=ans;
+    }
+public:
+    int numTrees(int n) {
+        vector<int> dp(n+1,-1);
+        return solve(n,dp);
+    }
+};
