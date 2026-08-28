9ccf2cb2123125df9d4564f6357e084ddaa02857
MOHIT GOSWAMI <mohitgoswami966@gmail.com>
2026-08-28 20:14:07 +0530

    Time: 36 ms (75.19%), Space: 11 MB (55.88%) - LeetHub

--- 0375-guess-number-higher-or-lower-ii/0375-guess-number-higher-or-lower-ii.cpp
+++ 0375-guess-number-higher-or-lower-ii/0375-guess-number-higher-or-lower-ii.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     int getMoneyAmount(int n) {
-        vector<vector<int>> dp(n+2,vector<int>(n+2,0));
+        vector<vector<int>> dp(n+2,vector<int>(n+1,0));
         for(int start=n;start>=1;start--){
             for(int end=start;end<=n;end++){
                 if(start==end) continue;
