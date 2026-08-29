ce87333d8a7e8948cea0e6611e55e00d87e61dd1
MOHIT GOSWAMI <mohitgoswami966@gmail.com>
2026-08-29 23:05:56 +0530

    Time: 51 ms (54.62%), Space: 54.6 MB (23.73%) - LeetHub

---
+++ 0044-wildcard-matching/0044-wildcard-matching.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    bool isMatch(string s, string p) {
+        vector<vector<int>> dp(s.size()+1,vector<int>(p.size()+1,0));
+        dp[0][0]=true;
+        for(int j=1;j<=p.size();j++){
+            bool flag=true;
+            for(int k=1;k<=j;k++){
+                if(p[k-1]!='*'){
+                    flag=false;
+                    break;
+                }
+            }
+            dp[0][j]=flag;
+        }
+        for(int i=1;i<=s.size();i++){
+            for(int j=1;j<=p.size();j++){
+                if(s[i-1]==p[j-1] || p[j-1]=='?'){
+                    dp[i][j]=dp[i-1][j-1];
+                }
+                else if(p[j-1]=='*'){
+                    dp[i][j]=(dp[i][j-1]||dp[i-1][j]);
+                }
+                else{
+                    dp[i][j]=false;
+                }
+            }
+        }
+        return dp[s.size()][p.size()];
+    }
+};
