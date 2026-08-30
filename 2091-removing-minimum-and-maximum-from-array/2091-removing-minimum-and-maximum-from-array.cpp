8382bd0cbc39d663bb32e7931e07859229ecd1b5
MOHIT GOSWAMI <mohitgoswami966@gmail.com>
2026-08-30 18:02:11 +0530

    Time: 0 ms (100%), Space: 92.1 MB (58.82%) - LeetHub

--- 2091-removing-minimum-and-maximum-from-array/2091-removing-minimum-and-maximum-from-array.cpp
+++ 2091-removing-minimum-and-maximum-from-array/2091-removing-minimum-and-maximum-from-array.cpp
@@ -1,32 +1,27 @@
 class Solution {
 public:
     int minimumDeletions(vector<int>& nums) {
-        int mini = INT_MAX;
-        int maxi = INT_MIN;
-        int miniIndex = 0;
-        int maxiIndex = 0;
-
-        for (int i = 0; i < nums.size(); i++) {
-            if (nums[i] < mini) {
-                mini = nums[i];
-                miniIndex = i;
+        int mini=INT_MAX;
+        int maxi=INT_MIN;
+        int miniINDEX=0;
+        int maxiINDEX=0;
+        for(int i=0;i<nums.size();i++){
+            mini=min(mini,nums[i]);
+            maxi=max(maxi,nums[i]);
+        }
+        for(int i=0;i<nums.size();i++){
+            if(nums[i]==mini){
+                miniINDEX=i;
             }
-
-            if (nums[i] > maxi) {
-                maxi = nums[i];
-                maxiIndex = i;
+            if(nums[i]==maxi){
+                maxiINDEX=i;
             }
         }
-
-        int n = nums.size();
-
-        int first = min(miniIndex, maxiIndex);
-        int last = max(miniIndex, maxiIndex);
-
-        int fromStart = last + 1;
-        int fromEnd = n - first;
-        int fromBoth = first + 1 + n - last;
-
-        return min({fromStart, fromEnd, fromBoth});
+        int n=nums.size();
+        int diffFromStart=max(miniINDEX,maxiINDEX)+1;
+        int diffFromEnd=n-min(miniINDEX,maxiINDEX);
+        int OneStartOneEnd=(min(miniINDEX, maxiINDEX) + 1)+ (n - max(miniINDEX, maxiINDEX));
+        int ans=min({diffFromStart,diffFromEnd,OneStartOneEnd});
+        return ans;
     }
 };
