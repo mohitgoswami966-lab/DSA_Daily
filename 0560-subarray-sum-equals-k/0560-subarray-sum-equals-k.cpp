df3f32c7e3ea3782553191aab7cb680892c450a6
MOHIT GOSWAMI <mohitgoswami966@gmail.com>
2026-09-08 16:50:38 +0530

    Time: 1799 ms (18.66%), Space: 34.4 MB (88.06%) - LeetHub

--- 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.cpp
+++ 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.cpp
@@ -4,7 +4,7 @@ public:
         int ans=0;
         int sum=0;
         for(int i=0;i<nums.size();i++){
-            sum=nums[i];
+            sum =nums[i];
             if(sum==k) ans++;
             for(int j=i+1;j<nums.size();j++){
                 sum+=nums[j];
