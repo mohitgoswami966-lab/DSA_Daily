f11f25749dce154bbf218156dade75944f11e8bd
MOHIT GOSWAMI <mohitgoswami966@gmail.com>
2026-09-08 15:12:36 +0530

    Time: 0 ms (100%), Space: 71.7 MB (52.52%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.cpp
+++ 0053-maximum-subarray/0053-maximum-subarray.cpp
@@ -5,8 +5,10 @@ public:
         int maxSum=INT_MIN;
         for(int i=0;i<nums.size();i++){
             sum+=nums[i];
-            maxSum=max(maxSum,sum);
-            if(sum<0) sum=0;
+            maxSum=max(sum,maxSum);
+            if(sum<0){
+                sum=0;
+            }
         }
         return maxSum;
     }
